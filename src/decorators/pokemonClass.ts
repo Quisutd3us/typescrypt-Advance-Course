f163968d8d7e8999ebd4ded8db8c6b1de59d92db
d3us <dnarino@gmail.com>
2023-11-01 18:25:13 -0500

    factory decorators

--- src/decorators/pokemonClass.ts
+++ src/decorators/pokemonClass.ts
@@ -2,7 +2,15 @@ function printToConsole(constructor:Function ){
     console.log(constructor)
 }
 
-@printToConsole
+const printToConsoleConditional = (doPrint:boolean =false): Function => {
+  if(doPrint){
+    return printToConsole;
+  }else{
+    return () => {};
+  }
+};
+
+@printToConsoleConditional(true)
 export class Pokemon {
   public publicAPI = "https://pokeapi.co";
   constructor(public name: string) {}
